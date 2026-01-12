e4f164db915a0c3f71a60c65621e59751a4c3028
Abhishek Arun <arun_abhishek@outlook.com>
2026-01-12 22:33:42 +0530

    Separated exercise from main code

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -4,61 +4,149 @@
 #include "Disk_Class/Disk.h"
 #include "FrontendInterface/FrontendInterface.h"
 
-int main(int argc, char *argv[]) {
-  /* Initialize the Run Copy of Disk */
+int main(int argc, char *argv[]) 
+{
   Disk disk_run;
-  
+
   RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
   HeadInfo relCatHeader;
   HeadInfo attrCatHeader;
 
   relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
 
-  for(int i = 0; i < relCatHeader.numEntries; i++)
+  for (int i = 0; i < relCatHeader.numEntries; i++) 
   {
-    Attribute relCatRecord[RELCAT_NO_ATTRS];
+    Attribute relCatRecord[RELCAT_NO_ATTRS]; 
     relCatBuffer.getRecord(relCatRecord, i);
 
     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
-    int attrBlock = ATTRCAT_BLOCK;
 
-    do  
-    {
-      RecBuffer attrCatBuffer(attrBlock);
-      attrCatBuffer.getHeader(&attrCatHeader);
+    for (int j = 0; j < attrCatHeader.numEntries; j++) {
 
-      for(int j = 0; j < attrCatHeader.numEntries; j++)
+      Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+      attrCatBuffer.getRecord(attrCatRecord, j);
+
+      if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0)
       {
-        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
-        attrCatBuffer.getRecord(attrCatRecord, j);
-
-        if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, "Students") == 0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Class") == 0)
-        {
-          strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Batch");
-          attrCatBuffer.setRecord(attrCatRecord, j);
-        }
-  
-        if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0)
-        {
           const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
           printf(" %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
-        }
       }
-      
-      attrBlock = attrCatHeader.rblock;
-
-    } while(attrCatHeader.rblock != -1);
+    }
 
     printf("\n");
 
-  } 
-
+  }
+  
   return 0;
-
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
-
-  // return FrontendInterface::handleFrontend(argc, argv);
-
 }
+
+// ----------------------------------------------------- EXERCISE-1 ----------------------------------------------------- //
+
+// int main(int argc, char *argv[]) {
+//   /* Initialize the Run Copy of Disk */
+//   Disk disk_run;
+  
+//   RecBuffer relCatBuffer(RELCAT_BLOCK);
+
+//   HeadInfo relCatHeader;
+//   HeadInfo attrCatHeader;
+
+//   relCatBuffer.getHeader(&relCatHeader);
+
+//   for(int i = 0; i < relCatHeader.numEntries; i++)
+//   {
+//     Attribute relCatRecord[RELCAT_NO_ATTRS];
+//     relCatBuffer.getRecord(relCatRecord, i);
+
+//     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+//     int attrBlock = ATTRCAT_BLOCK;
+
+//     do  
+//     {
+//       RecBuffer attrCatBuffer(attrBlock);
+//       attrCatBuffer.getHeader(&attrCatHeader);
+
+//       for(int j = 0; j < attrCatHeader.numEntries; j++)
+//       {
+//         Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+//         attrCatBuffer.getRecord(attrCatRecord, j);
+
+//         if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, "Students") == 0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Class") == 0)
+//         {
+//           strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Batch");
+//           attrCatBuffer.setRecord(attrCatRecord, j);
+//         }
+  
+//         if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0)
+//         {
+//           const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+//           printf(" %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+//         }
+//       }
+      
+//       attrBlock = attrCatHeader.rblock;
+
+//     } while(attrCatHeader.rblock != -1);
+
+//     printf("\n");
+
+//   } 
+
+//   return 0;
+
+//   // StaticBuffer buffer;
+//   // OpenRelTable cache;
+
+//   // return FrontendInterface::handleFrontend(argc, argv);
+
+// }
+
+// ----------------------------------------------------- EXERCISE-2 ----------------------------------------------------- //
+
+// int main(int argc, char *argv[]) 
+// {
+//   Disk disk_run;
+
+//   RecBuffer relCatBuffer(RELCAT_BLOCK);
+//   RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
+
+//   HeadInfo relCatHeader;
+//   HeadInfo attrCatHeader;
+
+//   relCatBuffer.getHeader(&relCatHeader);
+//   attrCatBuffer.getHeader(&attrCatHeader);
+
+//   for (int i = 0; i < relCatHeader.numEntries; i++) 
+//   {
+//     Attribute relCatRecord[RELCAT_NO_ATTRS]; 
+//     relCatBuffer.getRecord(relCatRecord, i);
+
+//     printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+//     for (int j = 0; j < attrCatHeader.numEntries; j++) {
+
+//       Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+//       attrCatBuffer.getRecord(attrCatRecord, j);
+      
+//       if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, "Students") == 0 && strcmp(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Class") == 0)
+//       {
+//         strcpy(attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, "Batch");
+//         attrCatBuffer.setRecord(attrCatRecord, j);
+//       }
+
+//       if(strcmp(attrCatRecord[ATTRCAT_REL_NAME_INDEX].sVal, relCatRecord[RELCAT_REL_NAME_INDEX].sVal) == 0)
+//       {
+//         const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+//         printf(" %s: %s\n", attrCatRecord[ATTRCAT_ATTR_NAME_INDEX].sVal, attrType);
+//       }
+//     }
+
+//     printf("\n");
+
+//   }
+  
+//   return 0;
+// }
