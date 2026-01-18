cc544ceea1d1b850930c90773e122f0b86c37add
Abhishek Arun <arun_abhishek@outlook.com>
2026-01-18 18:31:45 +0530

    Stage3 Excercises

--- NITCbase/mynitcbase/Cache/OpenRelTable.cpp
+++ NITCbase/mynitcbase/Cache/OpenRelTable.cpp
@@ -10,6 +10,8 @@ OpenRelTable::OpenRelTable()
         AttrCacheTable::attrCache[i] = nullptr;
     }
 
+    //-----------------------RELATION CACHE TABLE-------------------------------
+
     // Setting up the Relation Catalog relation in the Relation Cache Table
     RecBuffer relCatBlock(RELCAT_BLOCK);
     Attribute relCatRecord[RELCAT_NO_ATTRS];
@@ -35,8 +37,18 @@ OpenRelTable::OpenRelTable()
     *(RelCacheTable::relCache[ATTRCAT_RELID]) = relCacheEntry;
     // ----------------------------------------------------------------------------
 
+    // Setting up the Student relation in the Relation Cache Table
+    relCatBlock.getRecord(relCatRecord, RELCAT_SLOTNUM_FOR_ATTRCAT + 1);
+    RelCacheTable::recordToRelCatEntry(relCatRecord, &relCacheEntry.relCatEntry);
+    relCacheEntry.recId.block = RELCAT_BLOCK;
+    relCacheEntry.recId.slot = RELCAT_SLOTNUM_FOR_ATTRCAT + 1;
+
+    RelCacheTable::relCache[ATTRCAT_RELID + 1] = (struct RelCacheEntry*)malloc(sizeof(RelCacheEntry));
+    *(RelCacheTable::relCache[ATTRCAT_RELID + 1]) = relCacheEntry;
+    // ----------------------------------------------------------------------------
 
 
+    //-----------------------ATTRIBUTE CACHE TABLE-------------------------------
 
     //setting up the Relation Catalog relation in the Attribute Cache Table
     RecBuffer attrCatBlock(ATTRCAT_BLOCK);
@@ -73,7 +85,7 @@ OpenRelTable::OpenRelTable()
     attrCacheHead = nullptr;
     prev = nullptr;
 
-    for(int i = /*RELCAT_NO_ATTRS*/6; i < /*RELCAT_NO_ATTRS + ATTRCAT_NO_ATTRS*/ 12; i++)
+    for(int i = RELCAT_NO_ATTRS; i < RELCAT_NO_ATTRS + ATTRCAT_NO_ATTRS; i++)
     {
         AttrCacheEntry* attrCacheEntry = (AttrCacheEntry*)malloc(sizeof(AttrCacheEntry));
         attrCatBlock.getRecord(attrCatRecord, i);
@@ -95,6 +107,35 @@ OpenRelTable::OpenRelTable()
     }
 
     AttrCacheTable::attrCache[ATTRCAT_RELID] = attrCacheHead;
+    // ----------------------------------------------------------------------------
+
+
+    //setting up the Attribute Catalog relation in the Attribute Cache Table
+    attrCacheHead = nullptr;
+    prev = nullptr;
+
+    for(int i = RELCAT_NO_ATTRS + ATTRCAT_NO_ATTRS; i < RELCAT_NO_ATTRS + ATTRCAT_NO_ATTRS + 4; i++)
+    {
+        AttrCacheEntry* attrCacheEntry = (AttrCacheEntry*)malloc(sizeof(AttrCacheEntry));
+        attrCatBlock.getRecord(attrCatRecord, i);
+        AttrCacheTable::recordToAttrCatEntry(attrCatRecord, &attrCacheEntry->attrCatEntry);
+        attrCacheEntry->recId.block = ATTRCAT_BLOCK;
+        attrCacheEntry->recId.slot = i;
+        attrCacheEntry->next = nullptr;
+
+        if(attrCacheHead == nullptr)
+        {
+            attrCacheHead = attrCacheEntry;
+        }
+        else
+        {
+            prev->next = attrCacheEntry;
+        }
+
+        prev = attrCacheEntry;
+    }
+
+    AttrCacheTable::attrCache[ATTRCAT_RELID+1] = attrCacheHead;
 }
 
 OpenRelTable::~OpenRelTable() 
--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -12,7 +12,7 @@ int main(int argc, char *argv[]) {
 
   
 
-  for(int i = 0; i < 2; i++)
+  for(int i = 0; i < 3; i++)
   {
     RelCatEntry relCatBuffer;
     RelCacheTable::getRelCatEntry(i, &relCatBuffer);
